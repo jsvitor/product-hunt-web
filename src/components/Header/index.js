6b51f301e2151e192ec22d83f424c61d8139a851
jszvitor <jszvitor@gmail.com>
2020-06-11 17:37:22 -0300

    Create stateless component
    A way to make components with less verbose.

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+  <h1>Hello World</h1>
+); // stateless components, creating components with functions.
+
+/* 
+// is the same to say:
+class Header extends Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+} */
