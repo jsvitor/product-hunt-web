4175de952668a4d004df865c32f911061c3e7460
jszvitor <jszvitor@gmail.com>
2020-06-11 21:53:55 -0300

    Inserted pagination (prev/next).

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,7 +6,9 @@ import './styles.css';
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    page: 1,
   };
   
   
@@ -14,14 +16,37 @@ export default class Main extends Component {
     this.loadProducts()
   }
 
-  loadProducts = async () => {
-    const response = await api.get(`/products`)
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`)
+    console.log(response.data)
 
-    this.setState({ products: response.data.docs })
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page })
   }
 
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+    
+    this.loadProducts(pageNumber)
+  }
+  
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if ( page === productInfo.pages ) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  }
+  
   render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
 
     return (
       <div className="product-list">
@@ -33,6 +58,10 @@ export default class Main extends Component {
             <a href="#pablo">Acessar</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+        </div>
       </div>
     )
   }
